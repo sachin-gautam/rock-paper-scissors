09abec3b59b0d58376dc56b8a0d65fd120669239
sachin-gautam <gautamsachin757@gmail.com>
2024-07-25 19:35:14 +0545

    added the random computer choice generation

--- scripts.js
+++ scripts.js
@@ -1,1 +1,30 @@
-console.log("Hello World");
+console.log("Rock Paper Scissors");
+
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoices(){
+    rand = Math.floor(Math.random()*3);
+    switch(rand){
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "paper";
+            break;
+        case 2:
+            return "scissors";
+            break;
+    }
+}
+
+function getHumanChoices(){
+    let choice = prompt("Enter you choice");
+    return choice;
+}
+
+function playRound(humanChoice, computerChoice){
+    humanChoice = getHumanChoices().toLowerCase();
+    computerChoice = getComputerChoices();
+    
+}
